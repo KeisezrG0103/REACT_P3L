dd2101d90aa3264ff98563fdb4f9c0004a4c2201
KeisezrG0103 <210711228@students.uajy.ac.id>
2024-04-23 20:37:50 +0700

    fix sedikit bug di owner dan MO redirect page

--- src/utils/IsAuthRoute.jsx
+++ src/utils/IsAuthRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet} from 'react-router-dom';
-const IsAuthRoute = ({children}) => {
+const IsAuthRoute = () => {
     const karyawan_string = localStorage.getItem("karyawan");
     const karyawan = JSON.parse(karyawan_string);
 
