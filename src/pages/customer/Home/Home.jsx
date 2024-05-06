0b46b2374e275949f4aa9ac838df7ae6371395f6
KeisezrG0103 <210711228@students.uajy.ac.id>
2024-05-06 22:12:02 +0700

    ok

--- src/pages/customer/Home/Home.jsx
+++ src/pages/customer/Home/Home.jsx
@@ -221,7 +221,7 @@ const Home = () => {
                     <div className="flex flex-col gap-4 w-52">
                       <div className="skeleton h-32 w-full"></div>
                       <div className="skeleton h-4 w-28"></div>
-                      <div class="skeleton h-4 w-full"></div>
+                      <div className="skeleton h-4 w-full"></div>
                       <div className="skeleton h-4 w-full"></div>
                     </div>
                   </div>
