460535da6cf1706f6d49633ac69f69b3b4c8a4e2
KeisezrG0103 <210711228@students.uajy.ac.id>
2024-05-13 21:55:48 +0700

    refactor: Update Selesai.jsx component

--- src/pages/customer/Pembelian/Selesai/Selesai.jsx
+++ src/pages/customer/Pembelian/Selesai/Selesai.jsx
@@ -37,7 +37,7 @@ const Selesai = () => {
 
   return (
    
-    <div className="flex flex-col items-center justify-center space-y-4">
+    <div className="flex flex-col items-center justify-center gap-4">
       {pesananSelesai?.data.map((item) => (
         <div
           key={item.No_Nota}
